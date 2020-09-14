a49b0b1040adabd27d45e2b782754fae22bcebe7
ast-ral <ast.hyphen.ral@gmail.com>
2020-09-13 19:21:20 -0700

    implemented error correction, now I need to clean up, optimize, and document the code as well as see if I can make the API better

--- src/decoder.rs
+++ src/decoder.rs
@@ -2,7 +2,7 @@ use crate::field::Field;
 use crate::matrix::Matrix;
 use crate::polynomial::Polynomial;
 
-pub fn decode<T: Field>(elements: Vec<T::Element>, error_codewords: usize) -> Vec<usize> {
+pub fn decode<T: Field>(elements: &mut [T::Element], error_codewords: usize) -> &mut [T::Element] {
 	assert!(elements.len() <= T::SEQUENCE_LENGTH);
 	assert!(error_codewords < elements.len());
 
@@ -23,54 +23,89 @@ pub fn decode<T: Field>(elements: Vec<T::Element>, error_codewords: usize) -> Ve
 	}).collect();
 
 	if syndromes.iter().copied().all(T::is_zero) {
-		return Vec::new();
-		//return (&elements[error_codewords ..]).to_vec();
+		return &mut elements[error_codewords ..]
 	}
 
 	let can_locate = error_codewords / 2;
 
 	let syndrome_matrix = Matrix::new(
-		can_locate + 1,
 		can_locate,
+		can_locate + 1,
 		|y, x| {
 			syndromes[x + y]
 		},
 	);
 
-	let coefficients = reduce_matrix::<T>(syndrome_matrix);
+	let mut coefficients = vec![T::ONE];
+
+	reduce_matrix::<T, _>(syndrome_matrix, |elem| {
+		coefficients.push(T::neg(elem))
+	});
+
 	let error_locator = Polynomial::<T>(coefficients);
 
 	let inv_alpha = T::inv(T::ALPHA);
 	let mut inv_alpha_n = T::ONE;
 
-	let mut errors = Vec::with_capacity(error_locator.degree() - 1);
+	let mut locations = Vec::with_capacity(error_locator.degree() - 1);
+	let mut locators = Vec::with_capacity(error_locator.degree() - 1);
 
 	for i in 0 .. elements.len() {
 		let eval_on = inv_alpha_n;
 		inv_alpha_n = T::mul(inv_alpha_n, inv_alpha);
 		if T::is_zero(error_locator.eval(eval_on)) {
-			errors.push(i);
+			locations.push(i);
+			locators.push(T::inv(eval_on));
 		}
 	}
 
-	errors
+	let row_num = locations.len();
+	let row_len = locations.len() + 1;
+	let backing_len = row_num * row_len;
+
+	let mut backing = vec![T::ZERO; backing_len];
+
+	for (col, locator) in locators.into_iter().enumerate() {
+		let mut accum = T::ONE;
+
+		for i in 0 .. row_num {
+			backing[i * row_len + col] = accum;
+			accum = T::mul(accum, locator);
+		}
+	}
+
+	for row in 0 .. row_num {
+		backing[row * row_len + row_len - 1] = syndromes[row];
+	}
+
+	let correction_matrix = Matrix::from_raw(row_num, row_len, backing);
+
+	reduce_matrix::<T, _>(correction_matrix, |elem| {
+		let index = locations.pop().unwrap();
+		elements[index] = T::add(elements[index], T::neg(elem));
+	});
+
+	assert!(locations.len() == 0);
+
+	&mut elements[error_codewords ..]
 }
 
-/// Takes in a matrix, rrefs it and returns coefficients of the error polynomial.
-fn reduce_matrix<T: Field>(mut matrix: Matrix<T::Element>) -> Vec<T::Element> {
-	let mut errors = 0;
+/// Takes in a matrix, rrefs it and calls `push` for each variable that it solves for, in reverse order.
+fn reduce_matrix<T: Field, F: FnMut(T::Element)>(mut matrix: Matrix<T::Element>, mut push: F) {
+	// represents count of solvable variables
+	let mut solvable = 0;
 
 	let rows = matrix.rows();
 	let cols = matrix.cols();
 
 	// downward propagation
-	for i in 0 .. cols {
+	for i in 0 .. rows {
 		// verify the leading element is nonzero
 		if T::is_zero(*matrix.get(i, i)) {
 			let mut found = false;
-			for j in i + 1 .. cols {
+			for j in i + 1 .. rows {
 				if !T::is_zero(*matrix.get(j, i)) {
-					for k in 0 .. rows {
+					for k in 0 .. cols {
 						*matrix.get(i, k) = T::add(*matrix.get(i, k), *matrix.get(j, k));
 					}
 					found = true;
@@ -86,50 +121,48 @@ fn reduce_matrix<T: Field>(mut matrix: Matrix<T::Element>) -> Vec<T::Element> {
 		let leading = *matrix.get(i, i);
 		let inverse = T::inv(leading);
 
-		for j in i + 1 .. cols {
+		for j in i + 1 .. rows {
 			let factor = T::neg(T::mul(*matrix.get(j, i), inverse));
 
-			for k in i .. rows {
+			for k in i .. cols {
 				*matrix.get(j, k) = T::add(*matrix.get(j, k), T::mul(factor, *matrix.get(i, k)));
 			}
 		}
 
-		errors += 1;
+		solvable += 1;
 	}
 
-	let mut out = Vec::with_capacity(errors + 1);
-	out.push(T::ONE);
-
 	// upward propagation
-	for i in (0 .. errors).rev() {
-		let val = T::mul(*matrix.get(i, errors), T::inv(*matrix.get(i, i)));
-		//*matrix.get(i, errors) = val;
-		out.push(T::neg(val));
+	for i in (0 .. solvable).rev() {
+		let val = T::mul(*matrix.get(i, solvable), T::inv(*matrix.get(i, i)));
+		//*matrix.get(i, solvable) = val;
+		push(val);
 
 		for j in (0 .. i).rev() {
 			let subtracting = T::mul(val, *matrix.get(j, i));
-			*matrix.get(j, errors) = T::add(*matrix.get(j, errors), T::neg(subtracting));
+			*matrix.get(j, solvable) = T::add(*matrix.get(j, solvable), T::neg(subtracting));
 		}
 	}
-
-	out
 }
 
 #[test]
-fn test_error_location() {
+fn test_error_correction() {
 	use crate::gf256::GF256;
 	use crate::encoder::encode;
 
-	let msg = vec![0, 1, 2, 3, 4];
+	let msg: Vec<_> = (0 .. 25).collect();
 	let error_codewords = GF256::SEQUENCE_LENGTH - msg.len();
 
 	let mut encoded = encode::<GF256>(msg.clone(), error_codewords);
 
-	let errors: Vec<_> = (0 .. encoded.len()).step_by(3).collect();
+	let original_encoded = encoded.clone();
 
-	for i in errors.iter().copied() {
+	let error_locations: Vec<_> = (0 .. encoded.len()).step_by(3).collect();
+
+	for i in error_locations.into_iter() {
 		encoded[i] ^= 7;
 	}
 
-	assert!(decode::<GF256>(encoded, error_codewords) == errors);
+	assert!(decode::<GF256>(&mut encoded, error_codewords) == msg);
+	assert!(encoded == original_encoded);
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -6,10 +6,10 @@ pub struct Matrix<T> {
 
 impl<T> Matrix<T> {
 	pub fn get(&mut self, y: usize, x: usize) -> &mut T {
-		assert!(x < self.rows);
-		assert!(y < self.cols);
+		assert!(y < self.rows);
+		assert!(x < self.cols);
 
-		&mut self.backing[y * self.rows + x]
+		&mut self.backing[y * self.cols + x]
 	}
 
 	pub fn new<F: FnMut(usize, usize) -> T>(rows: usize, cols: usize, mut generator: F) -> Self {
@@ -18,8 +18,8 @@ impl<T> Matrix<T> {
 		let mut i = 0;
 
 		backing.resize_with(rows * cols, || {
-			let x = i % rows;
-			let y = i / rows;
+			let x = i % cols;
+			let y = i / cols;
 
 			i += 1;
 
@@ -35,5 +35,11 @@ impl<T> Matrix<T> {
 
 	pub fn cols(&self) -> usize {
 		self.cols
+	}
+
+	pub fn from_raw(rows: usize, cols: usize, backing: Vec<T>) -> Self {
+		assert!(backing.len() == rows * cols);
+
+		Matrix {rows, cols, backing}
 	}
 }
